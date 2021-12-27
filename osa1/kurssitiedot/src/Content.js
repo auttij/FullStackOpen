7e626a02abfb0f8e7084faab074dafc88fdf6e29
auttij1 <juhana.autti@gmail.com>
2021-12-27 14:01:35 +0200

    Refactored Content to use Part-components for 1.2

--- osa1/kurssitiedot/src/Content.js
+++ osa1/kurssitiedot/src/Content.js
@@ -1,17 +1,12 @@
 import React from 'react'
+import Part from './Part'
 
 const Content = (props) => {
 	return (
 		<>
-			<p>
-				{props.p1} {props.e1}
-			</p>
-			<p>
-				{props.p2} {props.e2}
-			</p>
-			<p>
-				{props.p3} {props.e3}
-			</p>
+			<Part course={props.p1} exercises={props.e1} />
+			<Part course={props.p2} exercises={props.e2} />
+			<Part course={props.p3} exercises={props.e3} />
 		</>
 	)
 }
---
+++ osa1/kurssitiedot/src/Part.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Part = (props) => {
+	return (
+		<p>{props.course} {props.exercises}</p>
+	)
+}
+
+export default Part
