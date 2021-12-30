dfd78dd0144e31063de8c12828803f4dfbeed40f
auttij1 <juhana.autti@gmail.com>
2021-12-30 09:14:55 +0200

    Added helper for total likes and tests for 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	const reducer = (sum, item) => {
+		return { likes: sum.likes + item.likes }
+	}
+
+	return blogs.reduce(reducer, { likes: 0 }).likes
+}
+
 module.exports = {
-	dummy
+	dummy, totalLikes
 }
