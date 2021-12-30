a97717a67ed8636e188ac6ed04e2f73d4375c565
auttij1 <juhana.autti@gmail.com>
2021-12-30 18:10:06 +0200

    Changed adding blogs to use token for 4.18/4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,16 +2,29 @@ const blogsRouter = require('express').Router()
 const { update } = require('lodash')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
-
-	const user = await User.findById('61cdc674827e867f01a83a7e')
+	const token = getTokenFrom(request)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' })
+	}
+	const user = await User.findById(decodedToken.id)
 
 	const blog = new Blog({
 		title: body.title,
