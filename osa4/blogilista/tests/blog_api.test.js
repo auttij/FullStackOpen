5828239e183734457960bea1b0ae60b2f2a4ad3d
auttij1 <juhana.autti@gmail.com>
2021-12-30 11:44:25 +0200

    Added test for POST /blogs/api/

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,36 +6,64 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-	await Blog.deleteMany({})
-	await Blog.insertMany(helper.initialBlogs)
+describe('GET /blogs/api/', () => {
+	beforeEach(async () => {
+		await Blog.deleteMany({})
+		await Blog.insertMany(helper.initialBlogs)
+	})
+
+	test('blogs are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all blogs are returned', async () => {
+		const blogsAtStart = await helper.blogsInDb()
+		expect(blogsAtStart).toHaveLength(helper.initialBlogs.length)
+	})
+
+	test('a specific blog is within the returned notes', async () => {
+		const blogsAtStart = await helper.blogsInDb()
+		const title = blogsAtStart.map(r => r.title)
+
+		expect(title).toContain(
+			'React patterns'
+		)
+	})
+
+	test('blog identifier field should be id', async() => {
+		const blogsAtStart = await helper.blogsInDb()
+		const id = blogsAtStart.map(r => r.id)
+		expect(id).toBeDefined()
+	})
 })
 
-test('blogs are returned as json', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/)
-})
+describe('POST /blogs/api', () => {
+	test('succeeds with valid data', async() => {
+		const newBlog = {
+			title: 'Canonical string reduction',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+			likes: 12,
+			__v: 0
+		}
 
-test('all blogs are returned', async () => {
-	const blogsAtStart = await helper.blogsInDb()
-	expect(blogsAtStart).toHaveLength(helper.initialBlogs.length)
-})
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
 
-test('a specific blog is within the returned notes', async () => {
-	const blogsAtStart = await helper.blogsInDb()
-	const title = blogsAtStart.map(r => r.title)
+		const blogsAtEnd = await helper.blogsInDb()
+		expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-	expect(title).toContain(
-		'React patterns'
-	)
-})
-
-test('blog identifier field should be id', async() => {
-	const blogsAtStart = await helper.blogsInDb()
-	const id = blogsAtStart.map(r => r.id)
-	expect(id).toBeDefined()
+		const titles = blogsAtEnd.map(n => n.title)
+		expect(titles).toContain(
+			'Canonical string reduction'
+		)
+	})
 })
 
 afterAll(() => {
