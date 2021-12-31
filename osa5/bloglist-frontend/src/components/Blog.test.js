c157c71bdbea5870f8288c6aa56c105048adab85
auttij1 <juhana.autti@gmail.com>
2021-12-31 11:49:19 +0200

    Added togglable tests, and added first Blog test for 5.13

---
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import Blog from './Blog'
+
+
+
+describe('<Blog />', () => {
+	let component
+
+	const updateBlog = jest.fn()
+	const deleteBlog = jest.fn()
+
+	const blog = {
+		'title':'Dependency Management With Python Poetry',
+		'author':'Philipp Acsany',
+		'url':'https://realpython.com/dependency-management-python-poetry/',
+		'likes':124,
+		'user':{
+			'username':'auttij',
+			'name':'Juhana Autti',
+			'id':'61cdc680827e867f01a83a82'
+		},
+		'id': '61cddd619f084491c99dc018'
+	}
+
+	const user = {
+		'username':'auttij',
+		'name':'Juhana Autti',
+		'id':'61cdc680827e867f01a83a82'
+	}
+
+	beforeEach(() => {
+		component = render(
+			<Blog blog={blog} updateBlog={updateBlog} deleteBlog={deleteBlog} user={user} />
+		)
+	})
+
+	test('renders blog title and author when not full visible', () => {
+		const hiddenBlogDiv = component.container.querySelector('.hiddenBlog')
+		const visibleBlogDiv = component.container.querySelector('.visibleBlog')
+
+		expect(hiddenBlogDiv).not.toHaveStyle('display: none')
+		expect(visibleBlogDiv).toHaveStyle('display: none')
+
+		expect(hiddenBlogDiv).toHaveTextContent(
+			'Dependency Management With Python Poetry'
+		)
+		expect(hiddenBlogDiv).toHaveTextContent(
+			'Philipp Acsany'
+		)
+		expect(hiddenBlogDiv).not.toHaveTextContent(
+			'https://realpython.com/dependency-management-python-poetry/'
+		)
+		expect(hiddenBlogDiv).not.toHaveTextContent(
+			'likes'
+		)
+		expect(hiddenBlogDiv).not.toHaveTextContent(
+			'Juhana Autti'
+		)
+	})
+})
