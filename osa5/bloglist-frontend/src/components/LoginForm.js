75443f4291ba8e23f8b8673005b19648bb1b773e
auttij1 <juhana.autti@gmail.com>
2021-12-31 10:46:31 +0200

    Added prop-types to LoginForm and Togglable

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -1,32 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = (props) => {
-  return (
-    <>
-      <h2>Login</h2>
-      <form onSubmit={props.onSubmit}>
-        <div>
-        username
-          <input
-          type="text"
-          value={props.username}
-          name="Username"
-          onChange={props.onUsernameChange}
-        />
-        </div>
-        <div>
-        password
-          <input
-          type="password"
-          value={props.password}
-          name="Password"
-          onChange={props.onPasswordChange}
-        />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </>
-  )
+	return (
+		<>
+			<h2>Login</h2>
+			<form onSubmit={props.onSubmit}>
+				<div>
+					username
+					<input
+						type="text"
+						value={props.username}
+						name="Username"
+						onChange={props.onUsernameChange}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type="password"
+						value={props.password}
+						name="Password"
+						onChange={props.onPasswordChange}
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</>
+	)
+}
+
+LoginForm.propTypes = {
+	handleSubmit: PropTypes.func.isRequired,
+	handleUsernameChange: PropTypes.func.isRequired,
+	handlePasswordChange: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired,
+	password: PropTypes.string.isRequired
 }
 
 export default LoginForm
