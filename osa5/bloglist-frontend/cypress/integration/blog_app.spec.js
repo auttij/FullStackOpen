781600035b82e7c5b5af80fd4ac6cf05099a14bd
auttij1 <juhana.autti@gmail.com>
2021-12-31 13:30:38 +0200

    Added login e2e tests

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -15,4 +15,41 @@ describe('Blog app', function() {
 		cy.get('#password')
 		cy.get('#login-button')
 	})
+
+	describe('Login', function() {
+		it('succeeds with correct credentials', function() {
+			cy.get('#username')
+				.type('auttij')
+
+			cy.get('#password')
+				.type('salainen')
+
+			cy.get('#login-button')
+				.click()
+
+			cy.get('h2')
+				.should('contain', 'Blog app')
+
+			cy.get('p')
+				.should('contain', 'Juhana Autti logged in')
+		})
+
+		it.only('fails with wrong credentials', function() {
+			cy.get('#username')
+				.type('auttij')
+
+			cy.get('#password')
+				.type('väärä')
+
+			cy.get('#login-button')
+				.click()
+
+			cy.get('.error')
+				.should('contain', 'wrong credentials')
+			cy.get('.error')
+				.should('have.css', 'color', 'rgb(255, 0, 0)')
+			cy.get('.error')
+				.should('have.css', 'border-style', 'solid')
+		})
+	})
 })
