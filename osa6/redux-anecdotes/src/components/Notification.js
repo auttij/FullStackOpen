ede4d23e542f067ac618a70b04daa2f4843fdbec
auttij1 <juhana.autti@gmail.com>
2022-01-01 10:30:18 +0200

    6.19 refactored Notification to use connect

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
-
+const Notification = ({ notification }) => {
 	const visible = { display: notification === '' ? 'none' : '' }
 
   const style = {
@@ -19,4 +17,11 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
