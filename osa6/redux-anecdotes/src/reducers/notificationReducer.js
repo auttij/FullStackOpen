a66909bf5605ba077f103e0f1b9ad16ba51db8f7
auttij1 <juhana.autti@gmail.com>
2022-01-01 08:01:16 +0200

    6.10 Added base for notificationReducer and moved to using combinedReducer

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const initialNotification = ''
+
+const notificationReducer = (state = initialNotification, action) => {
+	switch(action.type) {
+		default:
+			return state
+	}
+}
+
+export default notificationReducer
