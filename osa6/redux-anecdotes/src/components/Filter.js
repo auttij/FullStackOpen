8f0972701376fd46ba47e9ac40c3ad1441543dd4
auttij1 <juhana.autti@gmail.com>
2022-01-01 10:37:20 +0200

    6.20 refactored Filter and AnecdoteForm to use connect

--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
 
-const Filter = () => {
-	const dispatch = useDispatch()
-
+const Filter = (props) => {
 	const handleChange = (event) => {
-		dispatch(filterChange(event.target.value))
+		props.filterChange(event.target.value)
 	}
 	const style = {
 		marginBottom: 10
@@ -19,4 +17,9 @@ const Filter = () => {
 	)
 }
 
-export default Filter
+const mapDispatchToProps = {
+	filterChange
+}
+
+const connectedFilter = connect(null, mapDispatchToProps)(Filter)
+export default connectedFilter
